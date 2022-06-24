e8cd0d0ccdb4345cf56b0c690b41e2812211ec2e
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2022-06-25 00:47:48 +0300

    Create connect.go

---
+++ database/connect.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	"fmt"
+	"go-fiber-auth-jwt/config"
+	"go-fiber-auth-jwt/model"
+	"strconv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// ConnectDB connect to db
+func ConnectDB() {
+	var err error
+	p := config.Config("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	fmt.Println("Connection Opened to Database")
+	DB.AutoMigrate(&model.Product{}, &model.User{})
+	fmt.Println("Database Migrated")
+}
